caf9c35a96083b007fb46e2d21346c1634f66d02
swissmilo <milo.spirig@gmail.com>
2024-12-27 11:09:46 +0100

    add sitemap

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
 
 // Import routes
 const homeRouter = require('./routes/home');
@@ -10,6 +12,7 @@ const libraryRouter = require('./routes/library');
 const recommendationsRouter = require('./routes/recommendations');
 const sokoboxRouter = require('./routes/sokobox');
 const adminRouter = require('./routes/admin');
+
 
 const app = express();
 
@@ -29,6 +32,31 @@ app.use('/all-books', libraryRouter);
 app.use('/recommendations', recommendationsRouter);
 app.use('/sokobox', sokoboxRouter);
 app.use('/admin', adminRouter);
+
+/*
+ * sitemap
+ */
+app.get('/sitemap.xml', async (req, res) => {
+  const links = [
+    { url: '/', changefreq: 'weekly' },
+    { url: '/about', changefreq: 'weekly' },
+    { url: '/all-books', changefreq: 'weekly' },
+    { url: '/recommendations', changefreq: 'weekly' },
+    { url: '/sokobox', changefreq: 'weekly' }
+  ];
+
+  try {
+    const stream = new SitemapStream({ hostname: 'https://milo.run' });
+    res.header('Content-Type', 'application/xml');
+    
+    return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+      res.send(data);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
 
 app.listen(port, () => {
   console.log(`Recommendation app listening on port ${port}`);
