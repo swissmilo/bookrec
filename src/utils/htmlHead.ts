5948dc7146e96fe36e758d41951ce8a60c6b1290
swissmilo <milo.spirig@gmail.com>
2025-01-11 13:37:39 -0500

    add vercel analytics

--- src/utils/htmlHead.ts
+++ src/utils/htmlHead.ts
@@ -1,13 +1,19 @@
-export const getHtmlHead = (title: string): string => `
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Milo's personal website">
-    <meta name="author" content="Milo Spirig">
+export function getHtmlHead(title: string): string {
+  return `
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Milo's personal website">
+      <meta name="author" content="Milo Spirig">
 
-    <title>${title}</title>
-    
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
-    <link rel="stylesheet" href="/style.css">
-  </head>
-`;
+      <title>${title}</title>
+      
+      <link rel="icon" type="image/x-icon" href="/favicon.ico">
+      <link rel="stylesheet" href="/style.css">
+      <script defer src="https://unpkg.com/@vercel/analytics"></script>
+      <script>
+        window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+      </script>
+    </head>
+  `;
+}
